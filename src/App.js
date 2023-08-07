c1e67ec25cd5387bec141e7b570995c1e9b592da
ramakrishnahm <ramakrishna729m@gmail.com>
2023-08-07 10:29:33 +0530

    react-star-projects-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react"
+import data from './data'
+import List from "./List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const[people,setPeople] = useState(data)
+  return(
+    <section className="container">
+    <h3>{people.length} birthday today </h3>
+    <List people = {people}/>
+    <button onClick = {() => setPeople([])}>Remove all items</button>
+    </section>
+  )
 }
 
-export default App;
+export default App
