0c49d5f70d4c56c6e4d1d56ff29fd01a8580d0c6
ramakrishnahm <ramakrishna729m@gmail.com>
2023-08-07 10:35:56 +0530

    react-star-projects -01

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const[people,setPeople] = useState(data)
   return(
     <section className="container">
-    <h3>{people.length} birthday today </h3>
+    <h3>{people.length} birthdays today </h3>
     <List people = {people}/>
     <button onClick = {() => setPeople([])}>Remove all items</button>
     </section>
